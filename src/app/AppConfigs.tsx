2f598aa9eb81401fb705ee0bed3e2771ea31e747
Izhan Danish <danishraffik@gmail.com>
2026-05-17 07:21:43 +0800

    update query stale time

--- src/app/AppConfigs.tsx
+++ src/app/AppConfigs.tsx
@@ -5,8 +5,8 @@ import { QueryClientProvider } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
       retry: 1,
       staleTime: 1000 * 60 * 5,
     },
--- src/pages/admin/AdminView.tsx
+++ src/pages/admin/AdminView.tsx
@@ -36,7 +36,7 @@ const AdminView = () => {
             <AdminSidebar />
             <SidebarInset className="flex flex-col flex-1 ">
               <AdminTopbar />
-              <div className="px-4 md:px-6 py-6">
+              <div className="px-2 md:px-6 py-2 md:py-6">
                 <AnimatePresence mode="wait">
                   <ComponentFade key={activePage}>
                     {currentOutlet}
